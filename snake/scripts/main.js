c792bb6445e4ca6415c808bb5791a5c1f1213f4a
Your Name <you@example.com>
2022-04-11 11:28:34 +0800

    owo

--- snake/scripts/main.js
+++ snake/scripts/main.js
@@ -1,5 +1,8 @@
 var snake = document.getElementById('snake_head');
 var border = document.getElementById('border_object');
+var move_value_1 = 1;
+var move_value_2 = 1;
+var move_value_3 = snake;
 function move(way, increase_or_decrease, role) {
     if (way === 1) {
         if (increase_or_decrease === 1) {
@@ -14,24 +17,6 @@ function move(way, increase_or_decrease, role) {
             role.style.top = String(Number(role.style.top.slice(0, -2)) + 5).concat("px");
         }
         if (increase_or_decrease === -1) {
-            role.style.top = String(Number(role.style.top.slice(0, -2)) - 5).concat("px");
-        }
-    }
-}
-function anti_move(way, increase_or_decrease, role) {
-    if (way === 1) {
-        if (increase_or_decrease === -1) {
-            role.style.left = String(Number(role.style.left.slice(0, -2)) + 5).concat("px");
-        }
-        if (increase_or_decrease === 1) {
-            role.style.left = String(Number(role.style.left.slice(0, -2)) - 5).concat("px");
-        }
-    }
-    if (way === -1) {
-        if (increase_or_decrease === -1) {
-            role.style.top = String(Number(role.style.top.slice(0, -2)) + 5).concat("px");
-        }
-        if (increase_or_decrease === 1) {
             role.style.top = String(Number(role.style.top.slice(0, -2)) - 5).concat("px");
         }
     }
@@ -52,30 +37,53 @@ function setimage(key, role) {
             break;
     }
 }
-function movejudge(key, role) {
+function movejudge(key) {
     switch (key.keyCode) {
         case 37 || 68://L
-            move(1, -1, role);
+            move_value_1 = 1;
+            move_value_2 = -1;
             break;
         case 38 || 87://T
-            move(-1, -1, role);
+            move_value_1 = -1;
+            move_value_2 = -1;
             break;
         case 39 || 65://R
-            move(1, 1, role);
+            move_value_1 = 1;
+            move_value_2 = 1;
             break;
         case 40 || 83://D
-            move(-1, 1, role);
+            move_value_1 = -1;
+            move_value_2 = 1;
             break;
     }
 }
 function main(key, role) {
     setimage(key, role);
-    movejudge(key, role);
+    movejudge(key);
 }
+/*
+function position(key,role,border){
+    if(Number(role.style.top.slice(0,-2)) < 0){
+        move(-1,1,role);
+    }
+    if(Number(role.style.top.slice(0,-2)) > border.style.w){
+        move(-1,1,role);
+    }
+    if(Number(role.style.top.slice(0,-2)) < 0){
+        move(-1,1,role);
+    }
+    if(Number(role.style.top.slice(0,-2)) < 0){
+        move(-1,1,role);
+    }
+
+}
+*/
 var body = document.body;
 body.addEventListener('keydown', function (e) {
     main(e, snake);
 }, false) //偵測按下按鍵的行為
-
+while (true) {
+    move(move_value_1, move_value_2, move_value_3);
+}
 
 
